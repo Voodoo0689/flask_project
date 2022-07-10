233ed98b2b7ff60a4520561f74d993129319d6b9
Gavrilov <you@example.com>
2022-07-10 13:11:55 +0300

    'flask'

---
+++ flask_project/config.py
@@ -0,0 +1,9 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "voodoo06.07.89@gmail.com"
+    MAIL_PASSWORD = "qveacxmgckvbbdom"
